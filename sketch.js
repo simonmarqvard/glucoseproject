3ed0310a69b8d3ec4c7b6f934ed4ba565a91fa7f
Simon Jensen <Simonmarqvard@gmail.com>
2018-04-10 22:52:22 -0400

    response working

--- sketch.js
+++ sketch.js
@@ -1,102 +1,135 @@
 var ddata = []
+var timeslots;
 
 function initialize() {
-  //  getData();
-  buildDoughnutChart();
+  getData();
 }
 
-//
-// function getData() {
-//   $.ajax({
-//     url: "glucose.json",
-//     dataType: "json",
-//     error: function(err) {
-//       console.log(err)
-//     },
-//     success: function(data) {
-//       console.log("data is served")
-//       console.log(data)
-//     }
-//   })
+
+function getData() {
+  $.ajax({
+    url: "glucose2.json",
+    dataType: "json",
+    error: function(err) {
+      console.log(err)
+    },
+    success: function(data) {
+      window.data = data;
+      console.log("data is served");
+      //  console.log(data);
+      data.days.forEach(function(day) {
+        //console.log("YEAH");
+        //  calculatePercentages();
+        addDayMeasurement(day);
+      });
+    }
+  })
+}
+
+function buildDoughnutChart(day) {
+
+  day.measurements.forEach(function(measurement) {
+    var data = {
+      labels: [
+        "Hillary Clinton",
+        "Donald Trump",
+      ],
+      datasets: [{
+        data: [measurement["measurement_mgdl"]],
+        backgroundColor: [
+          "#179ee0",
+          "#fff",
+        ],
+        hoverBackgroundColor: [
+          "#1594d2",
+          "#f0563a",
+        ]
+      }]
+    };
+
+    // create chart options (this is optional)
+    // see list of options:
+    // http://www.chartjs.org/docs/latest/charts/doughnut.html
+    var options = {
+      legend: {
+        display: false,
+        position: 'bottom',
+        labels: {
+          fontColor: '#fff'
+        },
+      },
+      tooltips: {
+        backgroundColor: '#222',
+      },
+      animation: {
+        animateScale: false
+      }
+    }
+
+    // first, get the context of the canvas where we're drawing the chart
+    var ctx = document.getElementById("myChart").getContext("2d");
+
+    // now, create the doughnut chart, passing in:
+    // 1. the type (required)
+    // 2. the data (required)
+    // 3. chart options (optional)
+    myDoughnutChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: data,
+      options: options
+    });
+  });
+}
+
+// function calculatePercentages(day) {
+//   day.measurements.forEach(function(measurement) {
+//     var percentage = measurement["measurement_mgdl"] / 100;
+//     console.log(percentage);
+//   });
+//   return percentage;
 // }
 
-function buildDoughnutChart() {
 
 
-  var data = {
-    labels: [
-      "Hillary Clinton",
-      "Donald Trump",
-    ],
-    datasets: [{
-      data: [80, 20],
-      backgroundColor: [
-        "#179ee0",
-        "#fff",
-      ],
-      hoverBackgroundColor: [
-        "#1594d2",
-        "#f0563a",
-      ]
-    }]
-  };
+function addDayMeasurement(day) {
 
-  // create chart options (this is optional)
-  // see list of options:
-  // http://www.chartjs.org/docs/latest/charts/doughnut.html
-  var options = {
-    legend: {
-      display: false,
-      position: 'bottom',
-      labels: {
-        fontColor: '#fff'
-      },
-    },
-    tooltips: {
-      backgroundColor: '#222',
-    },
-    animation: {
-      animateScale: false
-    }
-  }
 
-  // first, get the context of the canvas where we're drawing the chart
-  var ctx = document.getElementById("myChart").getContext("2d");
+  //console.log(day);
+  var dayHtml = '<div class= dayColumn>';
 
-  // now, create the doughnut chart, passing in:
-  // 1. the type (required)
-  // 2. the data (required)
-  // 3. chart options (optional)
-  myDoughnutChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: data,
-    options: options
+  // add specific timeslots
+  day.measurements.forEach(function(measurement) {
+    // console.log(measurement["measurement_mgdl"])
+    var timeslot =
+      '<div class =' + measurement["label"] + '>' +
+      '<div class="description">' + measurement["measurement_mgdl"] + '</div>' +
+      '<div id="chart-container">' + '<canvas id="myChart" width="100%" height="100%">' +
+      '</canvas>' + '</div>' +
+      '</div>'
+    //concatenate string (push them together)
+    dayHtml += timeslot;
   });
 
-  var ctx = document.getElementById("my2Chart").getContext("2d");
+  dayHtml += '</div>';
 
-  // now, create the doughnut chart, passing in:
-  // 1. the type (required)
-  // 2. the data (required)
-  // 3. chart options (optional)
-  myDoughnutChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: data,
-    options: options
-  });
+  $('.days').append(dayHtml);
 
-  var ctx = document.getElementById("my3Chart").getContext("2d");
+  buildDoughnutChart(day);
 
-  // now, create the doughnut chart, passing in:
-  // 1. the type (required)
-  // 2. the data (required)
-  // 3. chart options (optional)
-  myDoughnutChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: data,
-    options: options
-  });
 }
+
+
+// function addTimeslot(currentday) {
+//   var timeslot =
+//     '<div class = "fasted">' +
+//     '<div class="description">' + 'HELLO' + '</div>' +
+//     '<div id="chart-container">' + '<canvas id="myChart" width="100%" height="100%">' +
+//     '</canvas>' + '</div>' +
+//     '</div>'
+//
+//   return $('.dayColumn').append(timeslot)
+//
+// }
 
 //
 // function setup() {
